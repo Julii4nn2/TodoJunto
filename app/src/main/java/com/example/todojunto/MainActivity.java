9f7d190a06599ff293f1552aff55a299597a8d31
Julii4nn2 <56120552+Julii4nn2@users.noreply.github.com>
2021-07-10 18:08:49 -0300

    TODO INTEGRADO, listo para seguir desarrollo

--- app/src/main/java/com/example/todojunto/MainActivity.java
+++ app/src/main/java/com/example/todojunto/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.location.Criteria;
 import android.location.GpsStatus;
 import android.location.Location;
@@ -15,6 +16,9 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -22,11 +26,13 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -37,6 +43,12 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
 
     Button btnGuardarExcel;
+    static final int REQUES_IMAGE_CAPTURE = 1;
+    private CameraPreview mPreview;
+    String currentPhotoPath;
+    private Camera mCamera;
+    private SurfaceHolder mHolder;
+    int bandera=0;
 
     private SensorManager sensorManager;
     private final float[] ac = new float[3];
@@ -60,21 +72,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     //Minimo tiempo para updates en Milisegundos
     private static final long MIN_TIEMPO_ENTRE_UPDATES = 1000; // 1 segundo
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // solicitar permisos para utilizar la camara
+        requestPermissions(new String[]{ Manifest.permission.CAMERA }, 1);
+        requestPermissions(new String[]{ Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
+        //genero un instanciado para la camara
+        mCamera = getCameraInstance(2);
+
+
+        //Creo una vista previa y le coloco el contenido de la actividad
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
         btnGuardarExcel = findViewById(R.id.btnGuardarExcel);
-        texto2 =  findViewById(R.id.texto2);
+        /*texto2 =  findViewById(R.id.texto2);
         texto3 =  findViewById(R.id.texto3);
-        texto4 =  findViewById(R.id.texto4);
+        texto4 =  findViewById(R.id.texto4);*/
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         btnGuardarExcel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 guardar();
+                bandera=0;
             }
         });
         // Get the location manager
@@ -119,6 +146,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         Boton1 = 1;
         Boton2 = 0;
+        bandera=1;
+        mCamera.takePicture(null,null, mPicture);
         //String NombreAchivo = new String("Datos_" + i + ".txt");
         File file = new File(getExternalFilesDir(null), "DATOS.txt");
         FileOutputStream outputStream;
@@ -126,8 +155,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         outputStream = new FileOutputStream(file);
         outputStream.write(textoASalvar.getBytes());
         outputStream.close();
-        texto2.setText("");
-        texto2.append("Se inicio la aplicación en el tiempo:"  + "\n" + new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss.SSSSSSS").format(new Date()));
+       //texto2.setText("");
+        //texto2.append("Se inicio la aplicación en el tiempo:"  + "\n" + new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss.SSSSSSS").format(new Date()));
+        Toast.makeText(getApplicationContext(), "INICIO DE TOMA DE DATOS", Toast.LENGTH_LONG).show();
+
 
     }
 
@@ -135,9 +166,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Boton1 = 0;
         Boton2 = 1;
 
-        texto3.setText("");
-        texto3.append("Se finalizo la toma de datos" + "\n" + new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss.SSSSSSS").format(new Date()) + "\n" + "La cantidad de datos son:" + N_dataset);
-        Toast.makeText(getApplicationContext(), "FIN DE TOMA DE DATOS", Toast.LENGTH_LONG).show();
+        //texto3.setText("");
+        //texto3.append("Se finalizo la toma de datos" + "\n" + new SimpleDateFormat("dd/MM/yyyy_HH:mm:ss.SSSSSSS").format(new Date()) + "\n" + "La cantidad de datos son:" + N_dataset);
+        Toast.makeText(getApplicationContext(), "FIN DE TOMA DE DATOS"+"\n" + "La cantidad de datos son:" + N_dataset, Toast.LENGTH_LONG).show();
 
     }
 
@@ -274,5 +305,132 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
     }
+
+    // Se comienza el desarrollo pra el objeto camara y manejarlo de manera manual
+// se consigue un instanciado de camra
+    public Camera getCameraInstance(int cameraselection){
+        Camera c = null;
+        try {
+            c = Camera.open(cameraselection); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            Toast.makeText(this, "La camara no esta disponible",Toast.LENGTH_LONG).show();
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+
+    // Genero una vista previa de lo que esta viendo la camara para poder hacer las capturas
+    public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+
+
+
+        public CameraPreview(Context context, Camera camera){
+            super(context);
+            mCamera = camera;
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+
+            mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+        }
+        @Override
+        public void surfaceCreated(@NonNull SurfaceHolder holder) {
+            // The Surface has been created, now tell the camera where to draw the preview.
+            try {
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            } catch (IOException e) {
+                //  Toast.makeText(this,"Error al configurar la vista previa",Toast.LENGTH_LONG).show();
+                //Log.d(TAG, "Error setting camera preview: " + e.getMessage()
+            }
+
+        }
+
+        @Override
+        public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+            // If your preview can change or rotate, take care of those events here.
+            // Make sure to stop the preview before resizing or reformatting it.
+
+            if (mHolder.getSurface() == null){
+                // preview surface does not exist
+                return;
+            }
+
+            // stop preview before making changes
+            try {
+                mCamera.stopPreview();
+            } catch (Exception e){
+                // ignore: tried to stop a non-existent preview
+            }
+
+            // set preview size and make any resize, rotate or
+            // reformatting changes here
+
+            // start preview with new settings
+            try {
+                mCamera.setPreviewDisplay(mHolder);
+                mCamera.startPreview();
+
+            } catch (Exception e){
+                //Toast.makeText(this,"Error al iniciar la vista previa",Toast.LENGTH_LONG).show();
+                //Log.d(Tag, "Error starting camera preview: " + e.getMessage());
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
+            mCamera.release();
+        }
+    }
+
+    // Se desarrolla la captura de imagenes
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = null;
+            try{
+                pictureFile = crearImagen();
+            }catch (IOException e){
+
+            }
+            if (pictureFile == null){
+//                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+                mCamera.startPreview();
+                if (bandera == 1){
+                    mCamera.takePicture(null,null, mPicture);
+                }
+            } catch (FileNotFoundException e) {
+//                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+//                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+        }
+
+    };
+
+    private File crearImagen() throws IOException {
+        String timeStamp = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss-ms").format(new Date());
+        String nombreImagen = "foto"+timeStamp+"_";
+        File directorio = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File imagen = File.createTempFile(nombreImagen , ".jpg",directorio);
+
+        currentPhotoPath = imagen.getAbsolutePath();
+        return imagen;
+    }
+    // FIN DEL DESARROLLO DE LA CAMARA MANUAL
+
 }
 
